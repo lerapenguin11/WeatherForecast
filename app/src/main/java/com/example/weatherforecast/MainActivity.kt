fa1bcec152a6d298ca72dc535c0afb7f8c411c99
lerap <lera.penguin01@mail.ru>
2022-10-20 03:33:29 +0500

    main activity

--- app/src/main/java/com/example/weatherforecast/MainActivity.kt
+++ app/src/main/java/com/example/weatherforecast/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.weatherforecast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.weatherforecast.databinding.ActivityMainBinding
+
+private lateinit var binding : ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        /*binding.mainCityNameTv.text = "Moscow"
+        binding.mainDateTv.text = "14 октября"
+        binding.mainWeatherConditionDescription.text = "Clear sky"
+        binding.mainTemp.text = "25\u00B0"
+        binding.mainTempMinTv.text = "18"
+        binding.mainTempMaxTv.text = "29"
+        binding.mainPressureMuTv.text = "1,4 hPa"
+        binding.mainHumidityMuTv.text = "87 %"
+        binding.mainWindSpeedMuTv.text = "5 m/s"
+        binding.mainSunriseMuTv.text = "6:20"
+        binding.mainSunsetMuTv.text = "22:50"
+        binding.mainWeatherConditionIcon.setImageResource(R.drawable.ic_sun)
+        binding.mainWeatherImage.setImageResource(R.mipmap.clowd3x)*/
     }
 }
