185210b778a22965306abdd4e4726e5a20695c94
lerap <lera.penguin01@mail.ru>
2022-10-25 16:50:08 +0500

    create BaseAdapter.kt

---
+++ app/src/main/java/com/example/weatherforecast/view/adapter/BaseAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.weatherforecast.view.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class BaseAdapter<D> : RecyclerView.Adapter<BaseAdapter.BaseViewHolder>()  {
+    //будет лежать данные с сервера
+    private val _mData by lazy { mutableListOf<D>() }
+    protected val mData : List<D> = _mData
+
+    override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
+        holder.bindView(position)
+    }
+
+    override fun getItemCount() = _mData.size
+
+    fun updateData(data : List<D>){
+        if(_mData.isEmpty() && (data.isNotEmpty())){
+            _mData.addAll(data)
+        }
+        else{
+            _mData.clear()
+            _mData.addAll(data)
+        }
+        notifyDataSetChanged()
+    }
+
+    abstract class BaseViewHolder(view : View) : RecyclerView.ViewHolder(view){
+        abstract fun bindView(position: Int)
+    }
+}
--- app/src/main/java/com/example/weatherforecast/view/adapter/MainHourlyListAdapter.kt
+++ app/src/main/java/com/example/weatherforecast/view/adapter/MainHourlyListAdapter.kt
@@ -1,28 +1,33 @@
 package com.example.weatherforecast.view.adapter
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.weatherforecast.R
 
+const val TAG = "RV-TEST"
 class MainHourlyListAdapter : RecyclerView.Adapter<MainHourlyListAdapter.HourlyViewHolder>() {
 
     override fun onCreateViewHolder(parent : ViewGroup, viewType : Int) : HourlyViewHolder {
+        Log.d(TAG, "-----------onCreateViewHolder: ")
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_main_hourly, parent, false)
 
         return HourlyViewHolder(view)
     }
 
     override fun onBindViewHolder(holderHourly: MainHourlyListAdapter.HourlyViewHolder, position: Int) {
-
+        Log.d(TAG, "onCreateViewHolder: ")
     }
 
     override fun getItemCount() : Int {
-        return 10
+        return 30
     }
 
     inner class HourlyViewHolder(view : View) : RecyclerView.ViewHolder(view) {
-
+        init {
+            Log.d(TAG, "HourlyViewHolder: ")
+        }
     }
 }
