76bc10e077ab727ebf5eed4b5b6262b2ec1c1308
lerap <lera.penguin01@mail.ru>
2022-10-28 20:10:33 +0500

    MainDailyListAdapter.kt

--- app/src/main/java/com/example/weatherforecast/view/adapter/MainDailyListAdapter.kt
+++ app/src/main/java/com/example/weatherforecast/view/adapter/MainDailyListAdapter.kt
@@ -1,12 +1,17 @@
 package com.example.weatherforecast.view.adapter
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import butterknife.BindView
 import com.example.weatherforecast.R
+import com.example.weatherforecast.business.model.DailyWeatherModel
+import com.google.android.material.textview.MaterialTextView
 
-class MainDailyListAdapter : RecyclerView.Adapter<MainDailyListAdapter.DailyHolderView>() {
+class MainDailyListAdapter : BaseAdapter<DailyWeatherModel>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DailyHolderView {
@@ -15,15 +20,31 @@ class MainDailyListAdapter : RecyclerView.Adapter<MainDailyListAdapter.DailyHold
         return DailyHolderView(view)
     }
 
-    override fun onBindViewHolder(holder: DailyHolderView, position: Int) {
+    @SuppressLint("NonConstantResourceId")
+    inner class DailyHolderView(view: View) : BaseViewHolder(view){
 
-    }
+        @BindView(R.id.item_daily_data_time)
+        lateinit var dailyTimeData : MaterialTextView
 
-    override fun getItemCount(): Int {
-        return 10
-    }
+        @BindView(R.id.item_daily_weather_condition_icon)
+        lateinit var icon : ImageView
 
-    inner class DailyHolderView(view: View) : RecyclerView.ViewHolder(view){
+        @BindView(R.id.item_daily_pop_tv)
+        lateinit var dailyPop : MaterialTextView
+
+        @BindView(R.id.item_daily_max_temp_tv)
+        lateinit var dailyMaxTemp : MaterialTextView
+
+        @BindView(R.id.item_daily_min_temp_tv)
+        lateinit var dailyMinTemp : MaterialTextView
+
+        override fun bindView(position: Int) {
+            dailyTimeData.text = "20 Saturday"
+            icon.setImageResource(R.drawable.ic_sun)
+            dailyPop.text = "25 %"
+            dailyMaxTemp.text = "26\u00B0"
+            dailyMinTemp.text = "19\u00B0"
+        }
 
     }
 }
