1931f7ec99fce3cc92dea46d049db04398ff2540
lerap <lera.penguin01@mail.ru>
2022-11-04 19:05:12 +0500

    realization retrofit2

---
+++ app/src/main/java/com/example/weatherforecast/business/api/WeatherApi.kt
@@ -0,0 +1,16 @@
+package com.example.weatherforecast.business.api
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherApi {
+
+    @GET("data/3.0/onecall?")
+    fun getWeatherForecast(
+        @Query("lat") lat : String,
+        @Query("lon") lon : String,
+        @Query("exclude") exclude : String = "minutely, alerts",
+        @Query("appid") appid : String = "7e987a61c1cd7c9c05a7c7f95cb703ee",
+        @Query("lang") lang : String = "en"
+    )
+}
